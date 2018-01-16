1c78e005513208635f133404a33a4dd7164ac43d
emgperez <em.gonzalez.perez@gmail.com>
2018-01-16 15:03:57 -0500

    Added RDS definition

--- terraform-Ansible2AWS/main.tf
+++ terraform-Ansible2AWS/main.tf
@@ -206,6 +206,20 @@ resource "aws_security_group" "RDS" {
 	}
 }
 
+# DB RDS
+resource "aws_db_instance" "db" {
+	allocated_storage = 10
+	engine = "mysql"
+	engine_version = "5.6.27"
+	instance_class = "${var.db_instance_class}"
+	name = "${var.dbname}"
+	username = "${var.dbuser}"
+	password = "${var.dbpassword}"
+	db_subnet_group_name = "${aws_db_subnet_group.rds_subnetgroup.name}"
+	vpc_security_group_ids = ["${aws_security_group.RDS.id}"]
+
+}
+
 # S3 code bucket
 
 # Compute
