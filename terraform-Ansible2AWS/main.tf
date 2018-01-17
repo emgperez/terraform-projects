e6b9a0a1b3b64ede7cd11d473d83da8c465cfe6d
emgperez <em.gonzalez.perez@gmail.com>
2018-01-17 04:36:42 -0500

    Added definition for the instance used in the ansible playbook

--- terraform-Ansible2AWS/main.tf
+++ terraform-Ansible2AWS/main.tf
@@ -300,6 +300,19 @@ resource "aws_key_pair" "auth" {
 }
 
 # Master dev server (with the initial code and the ansible playbook)
+resource "aws_instance" "dev" {
+	instance_type "${var.dev_instance_type}"
+	ami = "${var.dev_ami}"
+	tags 
+		Name = "dev"
+	}
+
+	key_name = "${aws_key_pair.auth.id}"
+	vpc_security_group_ids = ["${aws_security_group.public.id}"]
+	iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
+	subnet_id = "${aws_subnet.public.id}"
+}
+
 # LoadBalancer
 # AMI for the dev instance
 # Launch Configuration
