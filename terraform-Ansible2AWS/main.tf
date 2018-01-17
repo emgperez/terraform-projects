4846bbcbb5157e94093bf9a4ee52bda42193474f
emgperez <em.gonzalez.perez@gmail.com>
2018-01-17 06:12:26 -0500

    Added AMI and Launch Configuration Group definition

--- terraform-Ansible2AWS/main.tf
+++ terraform-Ansible2AWS/main.tf
@@ -366,8 +366,44 @@ resource "aws_elb" "prod" {
 	}
 }
 
-# AMI for the dev instance
+# AMI for the dev instance, our golden image
+resource "random_id" "ami" {
+	byte_length = 8
+	
+}
+
+resource "aws_ami_from_instance" "golden" {
+    # Includes cron job to retrieve code from the s3 bucket every 3 minutes
+    name = "ami-${random_id.ami.b64}"
+    source_instance_id = "${aws_instance.dev.id}"
+    provisioner "local-exec" {
+      command = <<EOT
+cat <<EOF > userdata
+#!/bin/bash
+/usr/bin/aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/
+/bin/touch /var/spool/cron/root
+sudo /bin/echo '*/5 * * * * aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/' >> /var/spool/cron/root
+EOF
+EOT
+  }
+}
+
+
+
 # Launch Configuration
+resource "aws_launch_configuration" "lc" {
+	name_prefix = "lc-"
+	image_id = "${aws_ami_from_instance.golden.id}"
+	instance_type = "${var.lc_instance_type}"
+	security_groups = ["${aws_security_group.private.id}"]
+	iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
+	key_name = "${aws_key_pair.auth.id}"
+	user_data = "${file("userdata")}"
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
 # Autoscaling group
 
 # Route53 records
