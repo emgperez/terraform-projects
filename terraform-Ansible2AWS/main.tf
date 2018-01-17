847fe18967201c6cd17c1030fe991e9d4c0501ff
emgperez <em.gonzalez.perez@gmail.com>
2018-01-17 04:40:49 -0500

    Added provisioners which add the dev instance to the ansible hosts and run the playbook wordpress.yml

--- terraform-Ansible2AWS/main.tf
+++ terraform-Ansible2AWS/main.tf
@@ -311,6 +311,24 @@ resource "aws_instance" "dev" {
 	vpc_security_group_ids = ["${aws_security_group.public.id}"]
 	iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
 	subnet_id = "${aws_subnet.public.id}"
+
+	# Write host and vars to ansible hosts file	
+	provisioner "local-exec" {
+      		command = <<EOD
+		cat <<EOF > aws_hosts 
+		[dev] 
+		${aws_instance.dev.public_ip} 
+		[dev:vars] 
+		s3code=${aws_s3_bucket.code.bucket} 
+		EOF
+		EOD
+  	}	
+
+	# Run the playbook
+  	provisioner "local-exec" {
+      		command = "sleep 6m && ansible-playbook -i aws_hosts wordpress.yml"
+  	}
+	
 }
 
 # LoadBalancer
