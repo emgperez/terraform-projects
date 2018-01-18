5528d32cb4f046a22fb0b7a1537925a62540c461
emgperez <em.gonzalez.perez@gmail.com>
2018-01-18 06:39:55 -0500

    Added initial public subnet definition

--- terraform-tests/template.tf
+++ terraform-tests/template.tf
@@ -1,11 +1,17 @@
 provider "aws" {
   region = "eu-central-1"
-
 }
 
 # VPC
 resource "aws_vpc" "my_vpc" {
-	cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+}
+
+# Public subnet
+resource "aws_subnet" "public" {
+	vpc_id = "${aws_vpc.my_vpc.id}"
+	cidr_block = "10.0.1.0/24"
+	
 }
 
 # EC2 instance configuration
@@ -17,5 +23,4 @@ resource "aws_vpc" "my_vpc" {
 #    Name = "hello-update_instance"
 #  }
 #}
-
 
