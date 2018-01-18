7d278e1ac79d107da608b036d031db662e7119a1
emgperez <em.gonzalez.perez@gmail.com>
2018-01-18 06:36:36 -0500

    Added initial VPC definition

--- terraform-tests/template.tf
+++ terraform-tests/template.tf
@@ -1,14 +1,21 @@
 provider "aws" {
-	region = "eu-central-1"
+  region = "eu-central-1"
 
+}
+
+# VPC
+resource "aws_vpc" "my_vpc" {
+	cidr_block = "10.0.0.0/16"
 }
 
 # EC2 instance configuration
-resource "aws_instance" "hello-instance" {
-	ami = "ami-b73b63a0"
-	instance_type = "t2.micro"
-	
-	tags {
-		Name = "hello_instance"
-	}
-}
+# resource "aws_instance" "hello-instance" {
+#  ami           = "ami-5652ce39"
+#  instance_type = "t2.micro"
+#
+#  tags {
+#    Name = "hello-update_instance"
+#  }
+#}
+
+
