c621e718d47d68c873f934934170110bc8e66004
emgperez <em.gonzalez.perez@gmail.com>
2018-01-29 03:56:52 -0500

    Added security group definition

--- terraform-tests/template.tf
+++ terraform-tests/template.tf
@@ -13,6 +13,29 @@ resource "aws_subnet" "public" {
   cidr_block = "10.0.1.0/24"
 }
 
+# Security group
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+  vpc_id      = "${aws_vpc.my_vpc.id}"
+
+  # Inbound rules
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # EC2 instance configuration
 resource "aws_instance" "hello-update-instance" {
   ami           = "ami-5652ce39"
@@ -24,20 +47,23 @@ resource "aws_instance" "hello-update-instance" {
 }
 
 resource "aws_instance" "master-instance" {
-  ami		= "ami-5652ce39"
-  instance_type	= "t2.micro"
-  subnet_id 	= "${aws_subnet.public.id}"
+  ami           = "ami-5652ce39"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.public.id}"
 }
 
 resource "aws_instance" "slave-instance" {
   ami           = "ami-5652ce39"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.public.id}"
+  subnet_id     = "${aws_subnet.public.id}"
+
   tags {
     master_hostname = "${aws_instance.master-instance.private_dns}"
   }
+
   lifecycle {
     ignore_changes = ["tags"] # Don't update slave instance if master instance is recreated
   }
+
   # depends_on = ["aws_instance.master-instance"]
 }
