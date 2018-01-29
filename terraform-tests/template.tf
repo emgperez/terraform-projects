569930a5488bd116cb15ab6c80a4f89041846328
emgperez <em.gonzalez.perez@gmail.com>
2018-01-29 04:00:03 -0500

    Added new EC2 instance

--- terraform-tests/template.tf
+++ terraform-tests/template.tf
@@ -67,3 +67,10 @@ resource "aws_instance" "slave-instance" {
 
   # depends_on = ["aws_instance.master-instance"]
 }
+
+resource "aws_instance" "mighty-trousers" {
+  ami                    = "ami-5652ce39"
+  instance_type          = "t2.micro"
+  subnet_id              = "${aws_subnet.public.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+}
