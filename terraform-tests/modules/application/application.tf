e8b2e16224dfc6e9034bf369e8b1aa6e937e2683
emgperez <em.gonzalez.perez@gmail.com>
2018-01-29 04:08:10 -0500

    Added application module

---
+++ terraform-tests/modules/application/application.tf
@@ -0,0 +1,30 @@
+# Application module
+
+resource "aws_security_group" "allow_http" {
+  name = "allow_http"
+  description = "Allow HTTP traffic"
+  vpc_id = "${aws_vpc.my_vpc.id}"
+
+  # Inbound rules
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rules
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "mighty-trousers" {
+  ami                    = "ami-5652ce39"
+  instance_type          = "t2.micro"
+  subnet_id              = "${aws_subnet.public.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+}
