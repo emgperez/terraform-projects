d440e49ee407466a61861c66bd7cbb9cafceca29
emgperez <em.gonzalez.perez@gmail.com>
2018-01-30 02:46:18 -0500

    Added 'vpc', 'subnet', 'name', 'environment' and 'instance_type' variables

--- terraform-tests/variables.tf
+++ terraform-tests/variables.tf
@@ -5,3 +5,15 @@ variable "region" {
   default = "eu-central-1"
 }
 
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "name" {}
+variable "environment" { default = "dev" }
+variable "instance_type" {
+  type = "map"
+  default = {
+    dev = "t2.micro"
+    test = "t2.medium"
+    prod = "t2.large"
+  }
+} 
