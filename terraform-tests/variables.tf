31881f560ac1dc88e649c2ed0b368e787cb77554
emgperez <em.gonzalez.perez@gmail.com>
2018-01-30 02:52:45 -0500

    Update variables file

--- terraform-tests/variables.tf
+++ terraform-tests/variables.tf
@@ -8,12 +8,17 @@ variable "region" {
 variable "vpc_id" {}
 variable "subnet_id" {}
 variable "name" {}
-variable "environment" { default = "dev" }
+
+variable "environment" {
+  default = "dev"
+}
+
 variable "instance_type" {
   type = "map"
+
   default = {
-    dev = "t2.micro"
+    dev  = "t2.micro"
     test = "t2.medium"
     prod = "t2.large"
   }
-} 
+}
