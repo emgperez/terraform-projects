fe47674f12e048eadc6701baa461f1aad9ebd6a1
emgperez <em.gonzalez.perez@gmail.com>
2018-01-31 02:42:46 -0500

    Added 'allow_ssh_access', 'vpc_cidr' and 'subnet_cidrs' variables

--- terraform-tests/variables.tf
+++ terraform-tests/variables.tf
@@ -10,7 +10,25 @@ variable "subnet_id" {}
 variable "name" {}
 
 variable "environment" {
-  default = "dev"
+  default = "prod"
+}
+
+variable "allow_ssh_access" {
+  description = "List of CIDR blocks that can access instances via SSH"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidrs" {
+  description = "CIDR blocks for public and private subnets"
+
+  default = {
+    public  = "10.0.1.0/24"
+    private = "10.0.2.0/24"
+  }
 }
 
 variable "instance_type" {
@@ -21,4 +39,8 @@ variable "instance_type" {
     test = "t2.medium"
     prod = "t2.large"
   }
+}
+
+variable "extra_sgs" {
+  default = []
 }
