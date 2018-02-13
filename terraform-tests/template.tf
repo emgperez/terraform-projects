82bb4200e0ecd7e97ec691e51e42c47b580553c0
Emilio <em.gonzalez.perez@gmail.com>
2018-02-13 08:43:21 +0100

    Modified public subnet definition with public ip mapping on launch

--- terraform-tests/template.tf
+++ terraform-tests/template.tf
@@ -18,6 +18,7 @@ resource "aws_vpc_peering_connection" "my_vpc_management" {
 resource "aws_subnet" "public" {
   vpc_id     = "${aws_vpc.my_vpc.id}"
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 # Call application module (security group + EC2 instance)
